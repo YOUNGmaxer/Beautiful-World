b2b92d83814192960a393f50019b3c23f6ec304d
YOUNGmaxer <517538470@qq.com>
2018-12-17 11:16:46 +0800

    webpack init

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  extends: 'airbnb-base/legacy',
+  parserOptions: {
+    // For ES9 syntax
+    ecmaVersion: 9,
+    // support ES modules
+    sourceType: 'module'
+  },
+  env: {
+    // for new ES6 global variables
+    es6: true
+  },
+  rules: {
+    "no-new": 0,
+    "no-console": 0
+  }
+}
---
+++ webpack.common.js
@@ -0,0 +1,90 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const VueLoaderPlugin = require('vue-loader/lib/plugin');
+
+module.exports = {
+  entry: {
+    main: './src/main.js',
+  },
+
+  resolve: {
+    alias: {
+      Views: path.resolve(__dirname, './src/views'),
+      Assets: path.resolve(__dirname, './src/assets'),
+      Styles: path.resolve(__dirname, './src/assets/styles')
+    }
+  },
+
+  module: {
+    rules: [
+      // 为了使用单文件组件，需要引入 vue-loader
+      {
+        test: /\.vue$/,
+        include: [
+          path.resolve(__dirname, 'src')
+        ],
+        use: [
+          'vue-loader'
+        ]
+      },
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader'
+        }
+      },
+      {
+        test: /\.scss/,
+        include: [
+          path.resolve(__dirname, 'src')
+        ],
+        use: [
+          'style-loader',
+          'css-loader',
+          'postcss-loader',
+          'sass-loader'
+        ]
+      },
+      {
+        test: /\.css/,
+        include: [
+          path.resolve(__dirname, 'src'),
+        ],
+        use: [
+          'style-loader',
+          'css-loader',
+          'postcss-loader'
+        ]
+      },
+      {
+        test: /\.(ttf|woff|woff2|eot)$/,
+        include: [
+          path.resolve(__dirname, 'node_modules/element-ui')
+        ],
+        loader: 'file-loader'
+      },
+      // 引用字体文件需要 url-loader
+      {
+        test: /\.(ttf|woff|woff2|eot)$/,
+        loader: 'url-loader?limit=100000',
+        exclude: /(node_modules)/
+      }
+    ]
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Beautiful World',
+      filename: 'index.html',
+      // template: './src/assets/template.html'
+    }),
+    // 将你定义过的其它规则复制并应用到 .vue 文件里相应语言的块。
+    new VueLoaderPlugin()
+  ],
+
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].js'
+  }
+};
