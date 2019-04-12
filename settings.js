99224578a1014db557d299704cfd3e6dfc34e225
YOUNGmaxer <517538470@qq.com>
2019-04-12 14:47:43 +0800

    数据库迁移，配置更改

--- settings.js
+++ settings.js
@@ -1,9 +1,11 @@
 // 保存项目中的一些通用静态配置信息，以此方便信息变化时统一修改
 
-const USER = encodeURIComponent('root');
+// const USER = encodeURIComponent('root');
+const USER = encodeURIComponent('realroot');
 const PWD = encodeURIComponent('688232max');
-const IP = '106.13.70.140';
-const PORT = '23333';
+// const IP = '106.13.70.140'; // 旧服务器（原百度云服务器）地址
+const IP = '114.115.130.213';
+const PORT = '27017';
 const authMechanism = 'SCRAM-SHA-1';
 const authSource = 'admin';
 // const MONGO_URL = `mongodb://${USER}:${PWD}@${IP}:${PORT}`;
